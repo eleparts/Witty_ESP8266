0d5189643e13f0cb3088f779b9804da51e86331a
eleparts <nyk.yeon@eleparts.co.kr>
2020-12-30 08:53:36 +0900

    들여쓰기 변경, 업데이트

--- 01.RGB_LED_Test/01.RGB_LED_Test.ino
+++ 01.RGB_LED_Test/01.RGB_LED_Test.ino
@@ -11,43 +11,43 @@ const int RGB_BULE    = 13;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  
-  pinMode(B_LED       , OUTPUT);         // builtin
-  pinMode(RGB_RED     , OUTPUT);         // red
-  pinMode(RGB_GREEN   , OUTPUT);         // green
-  pinMode(RGB_BULE    , OUTPUT);         // bule
-  
+	// put your setup code here, to run once:
+	
+	pinMode(B_LED       , OUTPUT);         // builtin
+	pinMode(RGB_RED     , OUTPUT);         // red
+	pinMode(RGB_GREEN   , OUTPUT);         // green
+	pinMode(RGB_BULE    , OUTPUT);         // bule
+	
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  digitalWrite(B_LED, HIGH);         // Builtin LED : GPIO voltage HIGH ==  LED off 
-  
-  RGB_LED(HIGH,LOW,LOW);             // RGB Red ON
-  delay(300);                        // delay 300ms
-  
-  RGB_LED(LOW,HIGH,LOW);             // RGB Green ON    
-  delay(300);                  
-  
-  RGB_LED(LOW,LOW,HIGH);             // RGB Bule ON
-  delay(300);  
+	// put your main code here, to run repeatedly:
+	
+	digitalWrite(B_LED, HIGH);         // Builtin LED : GPIO voltage HIGH ==  LED off 
+	
+	RGB_LED(HIGH,LOW,LOW);             // RGB Red ON
+	delay(300);                        // delay 300ms
+	
+	RGB_LED(LOW,HIGH,LOW);             // RGB Green ON    
+	delay(300);                  
+	
+	RGB_LED(LOW,LOW,HIGH);             // RGB Bule ON
+	delay(300);  
 
-  RGB_LED(LOW,LOW,LOW);              // RGB off
-  
-  digitalWrite(B_LED, LOW);          // Builtin LED : GPIO voltage LOW ==  LED on 
-  delay(500);
-  
-    
+	RGB_LED(LOW,LOW,LOW);              // RGB off
+	
+	digitalWrite(B_LED, LOW);          // Builtin LED : GPIO voltage LOW ==  LED on 
+	delay(500);
+	
+		
 }
 
 // RGB LED Control
 void RGB_LED(bool red, bool green, bool bule){
-  
-  digitalWrite(RGB_RED, red);   
-  digitalWrite(RGB_GREEN, green);   
-  digitalWrite(RGB_BULE, bule); 
+	
+	digitalWrite(RGB_RED, red);   
+	digitalWrite(RGB_GREEN, green);   
+	digitalWrite(RGB_BULE, bule); 
 
-  return;
+	return;
 }
--- 02.SW_LDR_Test/02.SW_LDR_Test.ino
+++ 02.SW_LDR_Test/02.SW_LDR_Test.ino
@@ -16,51 +16,51 @@ const int LDR            = A0; // photoresistor (CDS)
 
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(B_LED       , OUTPUT);         // builtin
-  pinMode(RGB_RED     , OUTPUT);         // red
-  pinMode(RGB_GREEN   , OUTPUT);         // green
-  pinMode(RGB_BULE    , OUTPUT);         // bule
+	// put your setup code here, to run once:
+	pinMode(B_LED       , OUTPUT);         // builtin
+	pinMode(RGB_RED     , OUTPUT);         // red
+	pinMode(RGB_GREEN   , OUTPUT);         // green
+	pinMode(RGB_BULE    , OUTPUT);         // bule
 
-  pinMode(PUSH_BUTTON , INPUT);
-  pinMode(FLASH_BUTTON, INPUT);
+	pinMode(PUSH_BUTTON , INPUT);
+	pinMode(FLASH_BUTTON, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  int sensorValue = analogRead(LDR);
-  int pushButton = digitalRead(PUSH_BUTTON);
-  int flashButton = digitalRead(FLASH_BUTTON);
+	// put your main code here, to run repeatedly:
+	
+	int sensorValue = analogRead(LDR);
+	int pushButton = digitalRead(PUSH_BUTTON);
+	int flashButton = digitalRead(FLASH_BUTTON);
 
-  // LDR(CDS)- RGB LED control
-  if(sensorValue > 900){          // CDS value 1024~901 : Red LED ON 
-    RGB_LED(HIGH,LOW,LOW);    
-    
-  }else if(sensorValue > 300){    // CDS value 900~301  : Bule LED ON 
-    RGB_LED(LOW,LOW,HIGH);  
-    
-  }else{ 
-    RGB_LED(LOW,HIGH,LOW);        // CDS value 300~     : Green 0LED ON 
-  }
+	// LDR(CDS)- RGB LED control
+	if(sensorValue > 900){          // CDS value 1024~901 : Red LED ON 
+		RGB_LED(HIGH,LOW,LOW);    
+		
+	}else if(sensorValue > 300){    // CDS value 900~301  : Bule LED ON 
+		RGB_LED(LOW,LOW,HIGH);  
+		
+	}else{ 
+		RGB_LED(LOW,HIGH,LOW);        // CDS value 300~     : Green 0LED ON 
+	}
 
-  if(!pushButton)                 // Push button - BUILTIN LED ON
-    digitalWrite(B_LED, LOW);
+	if(!pushButton)                 // Push button - BUILTIN LED ON
+		digitalWrite(B_LED, LOW);
 
-  if(!flashButton)                // Flash button - BUILTIN LED OFF
-    digitalWrite(B_LED, HIGH);
+	if(!flashButton)                // Flash button - BUILTIN LED OFF
+		digitalWrite(B_LED, HIGH);
 
-    
-  delay(200);
+		
+	delay(200);
 }
 
 
 // RGB LED Control
 void RGB_LED(bool red, bool green, bool bule){
-  
-  digitalWrite(RGB_RED, red);   
-  digitalWrite(RGB_GREEN, green);   
-  digitalWrite(RGB_BULE, bule); 
+	
+	digitalWrite(RGB_RED, red);   
+	digitalWrite(RGB_GREEN, green);   
+	digitalWrite(RGB_BULE, bule); 
 
-  return;
+	return;
 }
